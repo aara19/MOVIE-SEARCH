d162d8c3ded8214181e6957f34f0c55506cd9cfa
aara19 <srivastavaaaradhya2005@gmail.com>
2024-09-25 20:40:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const inputBox = document.querySelector('.inputBox');
 const getMovieInfo =  async (movie) => {
     try{
 const myAPIKey = "82dbe66";
-const url = `http://www.omdbapi.com/?apikey=${myAPIKey}&t=${movie}`;
+const url = `https://www.omdbapi.com/?apikey=${myAPIKey}&t=${movie}`;
 const response =  await fetch(url);
 if(!response.ok){
     throw new Error("Unable to fetch movie data.");
